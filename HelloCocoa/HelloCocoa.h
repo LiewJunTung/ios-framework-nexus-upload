3f4fc3417c8e26504524924d499917093ecbc685
JT <juntung@netvirta.com>
2018-01-30 18:05:17 +0800

    first commit

---
+++ HelloCocoa/HelloCocoa.h
@@ -0,0 +1,19 @@
+//
+//  HelloCocoa.h
+//  HelloCocoa
+//
+//  Created by Jun Tung Liew on 30/1/18.
+//  Copyright © 2018 JT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloCocoa.
+FOUNDATION_EXPORT double HelloCocoaVersionNumber;
+
+//! Project version string for HelloCocoa.
+FOUNDATION_EXPORT const unsigned char HelloCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloCocoa/PublicHeader.h>
+
+
---
+++ HelloCocoa/HelloCocoa.swift
@@ -0,0 +1,19 @@
+//
+//  HelloCocoa.swift
+//  HelloCocoa
+//
+//  Created by Jun Tung Liew on 30/1/18.
+//  Copyright © 2018 JT. All rights reserved.
+//
+
+import Foundation
+
+public class HelloCocoa {
+    public init (){
+        print("Class has been initialised")
+    }
+    
+    public func doSomething(){
+        print("Yeah, it works")
+    }
+}
